fc12b8b6c41496bf28fd61a69aeef57e38cd5139
mightynerd <staszw@gmail.com>
2019-12-25 19:50:47 +0100

    Set MAX_CONTENT_LENGHT to 5MiB

--- src/config.py
+++ src/config.py
@@ -4,6 +4,7 @@ curdir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = "verysecretXd"
+    MAX_CONTENT_LENGTH = 5 * 1024 * 1024
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(curdir, 'db.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(curdir, "static", "pr")
