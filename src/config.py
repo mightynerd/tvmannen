2e90e4c18b42e7ef50733309f5abec204cc44020
mightynerd <staszw@gmail.com>
2020-01-14 20:33:29 +0100

    Make PR time and PR fetch interval configurable in config.py

--- src/config.py
+++ src/config.py
@@ -9,3 +9,5 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(curdir, "static", "pr")
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'mp4'}
+    PR_TIME = 30        # Time for each PR in seconds
+    PR_FETCH_TIME = 120 # How often the PR list is fetched
