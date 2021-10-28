36574ea938f5eb9ac85d01bc9ed493f5b22ae448
Kirens <dev@erik.work>
2021-10-29 00:55:46 +0200

    Make variables configurable via environment

--- src/config.py
+++ src/config.py
@@ -3,11 +3,18 @@ curdir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = "verysecretXd"
-    MAX_CONTENT_LENGTH = 5 * 1024 * 1024
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(curdir, 'db.db')
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    MAX_CONTENT_LENGTH = int(os.getenv("MAX_CONTENT_LENGTH", 5 * 1024 * 1024))
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URI",
+        "sqlite:///" + os.path.join(curdir, "db.db"),
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(curdir, "static", "pr")
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'mp4'}
-    PR_TIME = 30        # Time for each PR in seconds
-    PR_FETCH_TIME = 120 # How often the PR list is fetched
+    UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", os.path.join(curdir, "static", "pr"))
+    ALLOWED_EXTENSIONS = set(
+        os.getenv("ALLOWED_EXTENSIONS", "png,jpg,jpeg,mp4").split(",")
+    )
+    # Time for each PR in seconds
+    PR_TIME = int(os.getenv("PR_TIME", 30))
+    # How often the PR list is fetched
+    PR_FETCH_TIME = int(os.getenv("PR_FETCH_TIME", 120))
